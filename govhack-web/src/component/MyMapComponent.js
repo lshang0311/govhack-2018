1fe79e96c33a29eee017930ac8e84d5f05967207
Ali Muzaffar <ali.muzaffar@bcgdv.com>
2018-09-08 11:12:11 +1000

    Remove google api token and add linting

--- govhack-web/src/component/MyMapComponent.js
+++ govhack-web/src/component/MyMapComponent.js
@@ -4,9 +4,9 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBtF7_d-uJHBsDSKEz03JChqKajMO3zVsY&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_TOKEN}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
+    containerElement: <div style={{ height: `100px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
